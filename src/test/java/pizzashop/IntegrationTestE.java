8c91c7b2cc9998868bf37d8fefd696fd476f1614
istratepaul.i@gmail.com <Constantin1>
2020-05-04 23:43:26 +0300

    integration test

--- src/test/java/pizzashop/IntegrationTestE.java
+++ src/test/java/pizzashop/IntegrationTestE.java
@@ -1,4 +1,65 @@
 package pizzashop;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pizzashop.model.MenuDataModel;
+import pizzashop.model.Payment;
+import pizzashop.model.PaymentType;
+import pizzashop.repository.MenuRepository;
+import pizzashop.repository.PaymentRepository;
+import pizzashop.service.PizzaService;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 public class IntegrationTestE {
+
+    private PizzaService service;
+
+    private PaymentRepository repoPay;
+
+    private MenuRepository repoMenu;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        MenuRepository repoMenu = new MenuRepository();
+        File directory = new File("./test");
+        if(!directory.exists()) {
+            directory.mkdir();
+        }
+        File file = new File(String.format("./test/payment-%d.txt", new Date().getTime()));
+        if(!file.exists()){
+            file.createNewFile();
+        }
+
+        PaymentRepository payRepo = new PaymentRepository(file);
+        payRepo.add(new Payment(1, PaymentType.Card, 3));
+        payRepo.add(new Payment(4, PaymentType.Cash, 13));
+        payRepo.add(new Payment(1, PaymentType.Card, 74));
+        service = new PizzaService(repoMenu, payRepo);
+        repoPay = payRepo;
+    }
+
+    @Test
+    public void testAddPay(){
+        service.addPayment(2,PaymentType.Cash,22);
+        assert repoPay.getAll().get(repoPay.getAll().size()-1).getTableNumber() == 2;
+    }
+
+    @Test
+    void testAmoutCash() {
+        assertEquals(service.getTotalAmount(PaymentType.Cash), 13);
+    }
+
+    @Test
+    void testAmoutCard() {
+        assertEquals(service.getTotalAmount(PaymentType.Card), 77);
+    }
 }
--- src/test/java/pizzashop/IntegrationTestR.java
+++ src/test/java/pizzashop/IntegrationTestR.java
@@ -4,14 +4,17 @@ import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pizzashop.model.MenuDataModel;
+import pizzashop.model.Payment;
+import pizzashop.model.PaymentType;
 import pizzashop.repository.MenuRepository;
 import pizzashop.repository.PaymentRepository;
 import pizzashop.service.PizzaService;
 
+import java.io.File;
 import java.util.Arrays;
+import java.util.Collections;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class IntegrationTestR {
 
@@ -24,16 +27,23 @@ public class IntegrationTestR {
     @BeforeEach
     public void setUp()
     {
-        repoPay = new PaymentRepository();
+        repoPay = spy(new PaymentRepository(new File("file.txt")));
         repoMenu = mock(MenuRepository.class);
         service = new PizzaService(repoMenu,repoPay);
     }
 
     @Test
-    public void test1(){
+    public void testMenuData(){
         MenuDataModel model1 = new MenuDataModel("ss",1,2.3);
         MenuDataModel model2 = new MenuDataModel("sss",2,2.5);
+        doNothing().when(repoPay).writeAll();
         when(repoMenu.getMenu()).thenReturn(Arrays.asList(model1,model2));
         assert service.getMenuData().size() == 2;
+    }
+    @Test
+    public void testTotalAmount(){
+        doNothing().when(repoPay).writeAll();
+        when(repoPay.getAll()).thenReturn(Collections.singletonList(new Payment(1, PaymentType.Cash, 22.2)));
+        assert service.getTotalAmount(PaymentType.Cash) == 22.2;
     }
 }
